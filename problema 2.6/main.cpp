8e78e79c297859eac65b0f3feea65530a9751a39
BratuRobertBeto <32935183+BratuRobertBeto@users.noreply.github.com>
2017-11-01 20:49:46 +0200

    Add files via upload

---
+++ problema 2.6/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int numaratoare;
+int main()
+{
+    cout << "numaratoarea este : " << endl ;
+
+
+    for( numaratoare=1 ; numaratoare<=12 ; numaratoare++ ){
+        cout << numaratoare ;
+        cout << " patratul sau: " ;
+        cout << numaratoare*numaratoare ;
+        cout << endl ;
+
+    }
+    return 0;
+}
