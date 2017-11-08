753f23fe028d7880d6252b5a43d3b746b76f0f8a
BratuRobertBeto <32935183+BratuRobertBeto@users.noreply.github.com>
2017-11-08 23:31:20 +0200

    Add files via upload

---
+++ Lab2problema 1.8 B.c
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int age ;
+    int movieLenght ;
+
+    cout << "Dati varsta : " ;
+    cin >> age ;
+    cout << endl ;
+    cout << "Dati durata filmului : " ;
+    cin >> movieLenght ;
+
+    if (! ( ( age>17 ) || ( movieLenght>90 ) ) )
+        cout <<"Dati discout " ;
+    else
+        cout <<"Nu dati discout " ;
+
+ return 0;
+
+}
